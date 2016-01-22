e912c43b23e71f1fe027a84d4bfadb78e559de2b
Daniel Russell <contactdanielrussell@gmail.com>
2016-01-22 11:10:50 -0800

    updated comment spacing.

--- nthFibonacci.js
+++ nthFibonacci.js
@@ -1,13 +1,8 @@
 // Suppose a newly-born pair of iguanas, one male, one female, are put in a large aquarium.
-
 // Iguanas are able to mate at the age of one month so that at the end of its second month a female can produce another pair of iguanas.
-
 // Suppose that our iguanas never die and that the female always produces one new pair (one male, one female) every month from the second month on.
-
 // How many pairs of iguanas will there be after n months?
-
 // For example, the iguana pair size for months zero through five are:
-
 // 0 1 1 2 3 5
 // If n were 4, your function should return 3; for 5, it should return 5.
 
